9f64712da6ea6db3f79dd71603a67d44fe3de882
Aryan Malik <149329741+aryanmalik28@users.noreply.github.com>
2024-11-18 22:51:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ app.use((req,res,next)=>{
 
 app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter );
-app.use("/",listingRouter );
+app.use("/",userRouter );
 
 app.all("*",(req,res,next)=>{
     next(new ExpressError(404,"Page not found"));
